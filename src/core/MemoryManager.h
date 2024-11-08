bd0e619b1b122e6e9b05ed4c36e1e9bf97fb85c0
ksk2kk <ksk2kk@outlook.com>
2024-11-08 19:49:29 +0800

    nm

--- src/core/MemoryManager.h
+++ src/core/MemoryManager.h
@@ -2,16 +2,49 @@
 #define MEMORYMANAGER_H
 
 #include <cstddef>
-namespace MyEngine{}
+#include <unordered_map>
+#include <memory>
+#include <mutex>
+
+namespace MyEngine {
+    class MemoryPool;
+
+    // 内存基类接口
 class Memory {
 public:
     virtual ~Memory() = default;
 
-    // 分配内存
+    // 分配内存的纯虚函数
     virtual void* AllocateMemory(std::size_t size) = 0;
 
-    // 释放内存
+    // 释放内存的纯虚函数
     virtual void FreeMemory(void* ptr, std::size_t size) = 0;
 };
+
+// 内存管理器类，继承 Memory 基类
+class MemoryManager : public Memory {
+public:
+    MemoryManager();
+    ~MemoryManager();
+
+    // 实现分配内存
+    void* AllocateMemory(std::size_t size) override;
+
+    // 实现释放内存
+    void FreeMemory(void* ptr, std::size_t size) override;
+
+private:
+    // 内存池的集合，根据大小进行管理
+    std::unordered_map<std::size_t, std::unique_ptr<MemoryPool>> memoryPools;
+    std::mutex memoryMutex;
+
+    // 获取合适的内存池
+    MemoryPool* GetPool(std::size_t size);
+
+    // 扩展内存池以满足需求
+    void ExpandPool(std::size_t size, std::size_t count);
+};
+
+} // namespace MyEngine
 
 #endif // MEMORYMANAGER_H
